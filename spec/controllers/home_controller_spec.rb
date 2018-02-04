340f8dede5335374dcfcf7e03d8d964c0908df4f
luis.bilecki <luis.bilecki@gmail.com>
2018-02-04 20:38:55 +0000

    Add a test in home controller

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
   describe 'GET index' do
+    before { get :index}
     it 'render index page' do
-      get :index
       expect(response).to render_template(:index)
     end
 
+    it 'should be success' do
+      expect(response).to be_success
+    end
   end
 end
